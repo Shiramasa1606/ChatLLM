36a08e9d4d19738288859b5549e73120e4a203de
Shiramasa <benjamin.correa.v@mail.pucv.cl>
2025-05-23 21:23:33 -0400

    Se actualiza BackEnd, se coloca ahora la conexion con el nuevo modelo

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "java-herencia-modelo")
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "java-herencia-model")
 
 app = FastAPI()
 
